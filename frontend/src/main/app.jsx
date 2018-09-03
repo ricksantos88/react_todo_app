638fea0a41d45b638ed2bd9d59afd2808936170b
Wendel Santos <henrique.santos1988@gmail.com>
2018-09-02 22:18:19 -0300

    Componentes de tela e implesmentação de rota simples.

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
+import Menu from '../template/menu';
+import Routes from './routes';
+
 
 export default (props) => (
-  <div className="container">
-    <h1>Teste - Frontend - TODO App</h1>
-  </div>
+    <div className="container">
+      <Menu />
+      <Routes />
+    </div>
 );
